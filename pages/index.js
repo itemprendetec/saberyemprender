4d1208a75a7cbdb7e51c02d340d92e8b24c63d9d
Smonsa <sssmonsalve@gmail.com>
2023-05-05 15:55:54 -0400

    Listo boton de exportar a excel

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useUser } from "../auth/useUser";
 import { useEffect, useState } from "react";
 import Sidebar from "@/components/Sidebar";
 import Head from "next/head";
+import * as XLSX from 'xlsx';
 
 const Private = ({ data }) => {
   function chunckArrayInGroups(arr, size) {
@@ -243,7 +244,7 @@ const Private = ({ data }) => {
                               </svg>
                               Add user
                             </button>
-                            <a
+                            <button onClick={()=>downloadExcel(data)}
                               href="#"
                               class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                             >
@@ -260,7 +261,7 @@ const Private = ({ data }) => {
                                 ></path>
                               </svg>
                               Export
-                            </a>
+                            </button>
                           </div>
                         </div>
                       </div>
@@ -534,6 +535,15 @@ const Private = ({ data }) => {
       </div>
     </>
   );
+};
+
+const downloadExcel = (data) => {
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  //let buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+  //XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
+  XLSX.writeFile(workbook, "Lista-Alumnos.xlsx");
 };
 
 export async function getServerSideProps() {
